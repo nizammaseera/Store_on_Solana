29eba44b4ec190c229ba3f8360923dd40332781b
Maseera Nizam <callmemasee@gmail.com>
2024-04-09 15:38:13 +0530

    first commit

---
+++ app/src/util/const.ts
@@ -0,0 +1,11 @@
+import { Connection, PublicKey } from "@solana/web3.js";
+
+
+export const CONNECTION = new Connection('https://api.devnet.solana.com', 'confirmed');
+
+export const PIZZA_PROGRAM_ID = new PublicKey('9v72LRyuGWQtrtNEkzRt7yLv8JvokMSY9vVy2Kn8Kkew');
+
+export const MERCHANT_WALLET = new PublicKey('va1yPZsd2qieP5pE6gtxvAHkHKEW3qmtoZy3oN1GcBX');
+
+export const SIMULATED_USDC_MINT = 'DfREi64eNGEgCuHjBynFSMaZktx4f5x34y4uswSA83ri';
+export const SIMULATED_BONK_MINT = 'EvLepoDXhscvLxbTQ7byj3NE6n6gSNJP3DeZx5k49uLm';
---
+++ app/src/util/order.ts
@@ -0,0 +1,79 @@
+import { SystemProgram, PublicKey } from "@solana/web3.js";
+import { Buffer } from "buffer";
+import { CONNECTION, PIZZA_PROGRAM_ID } from "./const";
+import { IDL } from "../idl/pizza_program";
+import { Program, Provider } from "@coral-xyz/anchor";
+
+export class PizzaOrder {
+  order: number;
+  pepperoni: number;
+  mushrooms: number;
+  olives: number;
+
+  constructor(props: {
+    order: number;
+    pepperoni: number;
+    mushrooms: number;
+    olives: number;
+  }) {
+    this.order = props.order;
+    this.pepperoni = props.pepperoni;
+    this.mushrooms = props.mushrooms;
+    this.olives = props.olives;
+  }
+}
+
+export const getOrderPublicKey = (orderNumber: number, payer: PublicKey) =>
+  PublicKey.findProgramAddressSync(
+    [
+      Buffer.from("solami_pizza"),
+      Buffer.from(Uint8Array.of(orderNumber)),
+      payer.toBuffer(),
+    ],
+    PIZZA_PROGRAM_ID
+  )[0];
+
+const createProgram = () => {
+  // create read-only Provider
+  const provider: Provider = {
+    connection: CONNECTION,
+  };
+
+  return new Program(IDL, PIZZA_PROGRAM_ID, provider);
+};
+
+// Create the write-order instruction (our custom program)
+export const createWriteOrderInstruction = async (
+  payer: PublicKey,
+  pizzaOrder: PizzaOrder
+) => {
+  const program = createProgram();
+  return await program.methods
+    .createPizzaOrder(
+      pizzaOrder.order,
+      pizzaOrder.pepperoni,
+      pizzaOrder.mushrooms,
+      pizzaOrder.olives
+    )
+    .accounts({
+      pizzaOrder: getOrderPublicKey(pizzaOrder.order, payer),
+      payer,
+      systemProgram: SystemProgram.programId,
+    })
+    .instruction();
+};
+
+// Display the order details from the on-chain data
+export const displayOnChainPizzaOrder = async (orderPublicKey: PublicKey) => {
+  console.log("displayOnChain...");
+  const program = createProgram();
+  console.log("got program!");
+
+  try {
+    return (await program.account.pizzaOrder.fetch(
+      orderPublicKey
+    )) as PizzaOrder;
+  } catch {
+    throw "Account data not found.";
+  }
+};
